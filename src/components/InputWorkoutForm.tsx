d77029d54fd27c1a8b9c88928f5063bbd44057df
Shiva <siddhartha.shiva18@gmail.com>
2024-02-08 14:12:45 -0500

    added useForm

--- src/components/InputWorkoutForm.tsx
+++ src/components/InputWorkoutForm.tsx
@@ -1,21 +1,40 @@
+import { Button } from "@chakra-ui/button";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
-import React from "react";
+import { useForm } from "react-hook-form";
+
+type FormInputs = {
+  workoutName: string;
+  category: string;
+  weight: number;
+  reps: number;
+  sets: number;
+};
 
 const InputWorkoutForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
   return (
-    <FormControl>
-      <FormLabel>Workout Name</FormLabel>
-      <Input type="workoutName" />
-      <FormLabel>Category</FormLabel>
-      <Input type="category" />
-      <FormLabel>Weight</FormLabel>
-      <Input type="weight" />
-      <FormLabel>Reps</FormLabel>
-      <Input type="reps" />
-      <FormLabel>Sets</FormLabel>
-      <Input type="sets" />
-    </FormControl>
+    <form onSubmit={handleSubmit((data: FormInputs) => console.log(data))}>
+      <FormControl>
+        <FormLabel>Workout Name</FormLabel>
+        <Input type="workoutName" {...register("workoutName")} />
+        <FormLabel>Category</FormLabel>
+        <Input type="category" {...register("category")} />
+        <FormLabel>Weight</FormLabel>
+        <Input type="weight" {...register("weight")} />
+        <FormLabel>Reps</FormLabel>
+        <Input type="reps" {...register("reps")} />
+        <FormLabel>Sets</FormLabel>
+        <Input type="sets" {...register("sets")} />
+        <Button marginTop={2} type="submit">
+          Submit
+        </Button>
+      </FormControl>
+    </form>
   );
 };
 
