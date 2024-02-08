1499aeadbbaf0814393b6aa5a29c17a8314d5e00
Shiva <siddhartha.shiva18@gmail.com>
2024-02-08 12:37:59 -0500

    Added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Button </Button>;
+  return (
+    <Grid templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "main main"` }}>
+      <GridItem area="nav" bg="coral">
+        My Fitness Tracker - nav
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
